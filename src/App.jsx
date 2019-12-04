21becff51df7a7cc7cc6ed0836d437c855894225
Achyp14 <achyp14@gmail.com>
2019-12-04 22:10:07 +0200

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import './styles/common-styles.scss';
+import './styles/icons.scss';
 
-const App = () => (<div>Kvest</div>);
+const App = () => (
+    <div className="app-container">
+        <>Kvest</>
+    </div>
+);
 
 export default App;
